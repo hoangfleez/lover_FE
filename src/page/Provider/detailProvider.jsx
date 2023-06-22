e7a013fb3d1b9c276b34616ac9e70db74d25d5b8
hoangfsama <leehoagf@gmail.com>
2023-06-22 13:41:10 +0700

    sua giao dien thuer

--- src/page/Provider/detailProvider.jsx
+++ src/page/Provider/detailProvider.jsx
@@ -1,104 +1,244 @@
 import { Link, useParams } from "react-router-dom";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProviderDetail } from "../../services/providerService";
-import { Box, Button, CardMedia, Container, Divider, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CardMedia,
+  Chip,
+  Container,
+  Divider,
+  Rating,
+  Stack,
+  Typography,
+} from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import moment from "moment";
 import Rent from "../booking/Rent.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Evaluate from "./Evaluate";
 
 const DetailProvider = () => {
-    const { id } = useParams();
-    const dispatch = useDispatch();
-    const [showRent, setShowRent] = useState(false);
-    const [dataProvider, setDataProvider] = useState({});
-    const detailProviderDetail = useSelector((state) => {
-        return state.provider.showOneProvider;
-    });
-    console.log(detailProviderDetail,99999)
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const [showRent, setShowRent] = useState(false);
+  const [dataProvider, setDataProvider] = useState({});
+  const detailProviderDetail = useSelector((state) => {
+    return state.provider.showOneProvider;
+  });
+  console.log(detailProviderDetail, 99999);
 
-    const handleClose = () => {
-        setShowRent(false);
-    };
+  const handleClose = () => {
+    setShowRent(false);
+  };
 
+  const handleRentProvider = (provider) => {
+    setDataProvider(provider);
+    setShowRent(true);
+  };
 
-
-    const handleRentProvider = (provider) => {
-        setDataProvider(provider);
-        setShowRent(true)
-    }
-
-
-
-    useEffect(() => {
-        dispatch(getProviderDetail(id));
-    }, []);
-    return (
+  useEffect(() => {
+    dispatch(getProviderDetail(id));
+  }, []);
+  return (
+    <Box
+      sx={{
+        marginTop: "64px",
+        padding: "10px",
+        height: "100%",
+        backgroundColor: "customColorSchemes.bgColorPage",
+      }}
+    >
+      <Container maxWidth="xl">
         <Box
-            sx={{
-                marginTop: "64px",
-                padding: "10px",
-                height: "93vh",
-            }}
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "100%",
+            padding: "20px 0",
+          }}
         >
-            <Container maxWidth="lg">
-                <Box sx={{ display: "flex", width: "100%", height: "100%" }}>
-                    <Box sx={{ width: "20%" }}>
-                        <CardMedia
-                            component="img"
-                            sx={{ width: 250}}
-                            image={detailProviderDetail.avatar}
-                            alt="Avatar"
-                        />
-                        <Typography component="h2">
-                            Ngày tham gia:{" "}
-                            {moment(detailProviderDetail.joinDate).format("DD/MM/YYYY")}
-                        </Typography>
-                        <Link to={detailProviderDetail.linkFB}>
-                            <FacebookIcon sx={{ color: "blue", fontSize: "60px" }} />
-                        </Link>
-                        <div> Giới tính: {detailProviderDetail.sex}</div>
-                        <div> Sinh nhật: {detailProviderDetail.dob}</div>
-                        <div> Quốc tịch: {detailProviderDetail.country}</div>
-                        <div> Sở thích: {detailProviderDetail.hobby}</div>
-                        <div> Chiều cao: {detailProviderDetail.height}</div>
-                        <div> Cân nặng: {detailProviderDetail.weight}</div>
-                    </Box>
-                    <Box sx={{ width: "60%", padding: "0 40px" }}>
-                        <Typography variant="h3">{detailProviderDetail.name}</Typography>
-                        <Divider />
-                        <div> Mô tả bản thân: {detailProviderDetail.desc}</div>
-
-                    </Box>
-                    <Box sx={{ width: "20%" }}>
-                        <Box
-                            sx={{
-                                width: 250,
-                                border: "1px solid grey",
-                                borderRadius: "15px",
-                                padding: "10px"
-                            }}
-                        >
-                            <Typography variant="h4" gutterBottom sx={{color:"red"}}>
-                                {detailProviderDetail.price} đ/1h
-                            </Typography>
-                            <Box sx={{flexWrap:"wrap", gap:"15px", display:"flex"}}>
-                                <Button onClick={() => handleRentProvider(detailProviderDetail)} fullWidth variant="contained">THUÊ</Button>
-                                <Button fullWidth variant="contained">DONET</Button>
-                                <Button fullWidth variant="contained">Chat</Button>
-                            </Box>
-                        </Box>
-                    </Box>
-                </Box>
-            </Container>
-            <Rent
-            show={showRent}
-            handleClose={handleClose}
-            dataProvider={dataProvider}
+          <Box
+            sx={{
+              width: "25%",
+              alignItems: "center",
+              display: "flex",
+              flexDirection: "column",
+              margin: "10px",
+              gap: "10px",
+            }}
+          >
+            <CardMedia
+              component="img"
+              sx={{ width: 250, height: 250, borderRadius: 5 }}
+              image={detailProviderDetail.avatar}
+              alt="Avatar"
             />
+            {detailProviderDetail.ready == "on" ? (
+              <Typography variant="h5" gutterBottom sx={{ color: "green" }}>
+                Đang sẵn sàng
+              </Typography>
+            ) : (
+              <Typography variant="h5" gutterBottom sx={{ color: "gray" }}>
+                Không sẵn sàng
+              </Typography>
+            )}
+
+            <Link to={detailProviderDetail.linkFB}>
+              <FacebookIcon sx={{ color: "blue", fontSize: "40px" }} />
+            </Link>
+            <Typography variant="button" display="block" gutterBottom>
+              NGÀY THAM GIA:
+              {moment(detailProviderDetail.joinDate).format("DD/MM/YYYY")}
+            </Typography>
+          </Box>
+          <Box sx={{ width: "60%", padding: "0 10px" }}>
+            <Typography variant="h3">{detailProviderDetail.name}</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: "120px",
+              }}
+            >
+              <Typography variant="button" color={"gray"}>
+                ĐÃ DƯỢC THUÊ
+              </Typography>
+              <Typography variant="button" color={"red"}>
+                {detailProviderDetail.count}/người
+              </Typography>
+            </Box>
+            <Divider />
+            <Box>
+              Dịch vụ chính
+              <Stack direction="row" p={2}>
+                <Chip label="Chip Filled" />
+              </Stack>
+            </Box>
+            <Box>
+              Dịch vụ mở rộng
+              <Stack direction="row" spacing={1} p={2}>
+                <Chip label="Chip Filled" />
+                <Chip label="Chip Outlined" variant="outlined" />
+              </Stack>
+            </Box>
+            <Divider />
+            <Box p={1}>
+              <Typography variant="h5">Thông tin</Typography>
+              <Typography variant="body2">
+                {detailProviderDetail.desc}
+              </Typography>
+              <Box
+                sx={{ width: "100%", height: "150px", border: "1px solid red" }}
+              >
+                nơi chứa ảnh tự sướng
+              </Box>
+              <Typography variant="body2">
+                {" "}
+                Giới tính: {detailProviderDetail.sex}
+              </Typography>
+              <Typography variant="body2">
+                {" "}
+                Sinh nhật: {detailProviderDetail.dob}
+              </Typography>
+              <Typography variant="body2">
+                {" "}
+                Quốc tịch: {detailProviderDetail.country}
+              </Typography>
+              <Typography variant="body2">
+                {" "}
+                Sở thích: {detailProviderDetail.hobby}
+              </Typography>
+              <Typography variant="body2">
+                {" "}
+                Chiều cao: {detailProviderDetail.height}
+              </Typography>
+              <Typography variant="body2">
+                {" "}
+                Cân nặng: {detailProviderDetail.weight}
+              </Typography>
+            </Box>
+            <Divider />
+            <Evaluate />
+          </Box>
+          <Box sx={{ width: "20%" }}>
+            <Box
+              sx={{
+                width: 280,
+                border: "1px solid grey",
+                borderRadius: "15px",
+                padding: "10px",
+              }}
+            >
+              <Typography variant="h4" gutterBottom sx={{ color: "red" }}>
+                {detailProviderDetail.price} đ/1h
+                <Rating readOnly />
+              </Typography>
+              <Box sx={{ flexWrap: "wrap", gap: "15px", display: "flex" }}>
+                <Button
+                  onClick={() => handleRentProvider(detailProviderDetail)}
+                  fullWidth
+                  variant="contained"
+                  sx={{
+                    p: 1.5,
+                    bgcolor: "red",
+                    color: "white",
+                    "&:hover": {
+                      bgcolor: "red",
+                      color: "white",
+                      boxShadow: "none",
+                    },
+                  }}
+                >
+                  THUÊ
+                </Button>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  sx={{
+                    p: 1.5,
+                    bgcolor: "white",
+                    color: "black",
+                    "&:hover": {
+                      bgcolor: "white",
+                      boxShadow: "none",
+                      color: "black",
+                    },
+                  }}
+                >
+                  DONET
+                </Button>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  sx={{
+                    p: 1.5,
+                    bgcolor: "white",
+                    color: "black",
+                    "&:hover": {
+                      bgcolor: "white",
+                      boxShadow: "none",
+                      color: "black",
+                    },
+                  }}
+                >
+                  <ChatBubbleIcon />
+                  Chat
+                </Button>
+              </Box>
+            </Box>
+          </Box>
         </Box>
-    );
+      </Container>
+      <Rent
+        show={showRent}
+        handleClose={handleClose}
+        dataProvider={dataProvider}
+      />
+    </Box>
+  );
 };
 
 export default DetailProvider;
--- src/page/booking/Rent.jsx
+++ src/page/booking/Rent.jsx
@@ -1,156 +1,252 @@
 import React, { useEffect, useState } from "react";
-import { Modal, Button } from "react-bootstrap";
 import { addBooking } from "../../services/bookingService.js";
 import { useDispatch } from "react-redux";
 import Swal from "sweetalert2";
 import PlacesAutocomplete, {
-    geocodeByAddress,
-    getLatLng,
+  geocodeByAddress,
+  getLatLng,
 } from "react-places-autocomplete";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Divider, Grid, MenuItem, Select, Stack } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "30%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "customColorSchemes.backgroundColor",
+  boxShadow: 24,
+  borderRadius: "15px",
+};
 
 const Rent = (props) => {
-    const dispatch = useDispatch();
-    const { show, handleClose, dataProvider } = props;
-    const [address, setAddress] = useState("");
-    const [selectedOption, setSelectedOption] = useState("");
-    const [rent, setRent] = useState({});
-    const [totalCost, setTotalCost] = useState(0);
-    const [scriptLoaded, setScriptLoaded] = useState(false);
+  const dispatch = useDispatch();
+  const { show, handleClose, dataProvider } = props;
+  const [address, setAddress] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+  const [rent, setRent] = useState({});
+  const [totalCost, setTotalCost] = useState(0);
+  const [scriptLoaded, setScriptLoaded] = useState(false);
 
-    const handleRentProvider = async () => {
-        const newRent = {
-            address: address,
-            selectedOption: selectedOption,
-        };
-        setRent(newRent);
-        let data = dispatch(
-            addBooking({ providerId: dataProvider.id, bookingData: newRent })
-        );
-
-        if (data) {
-            Swal.fire({
-                icon: "success",
-                title: "Đã thuê! Vui lòng chờ người cho thuê phản hồi.",
-                showConfirmButton: false,
-                timer: 3000,
-            });
-        } else {
-            // Handle failure case
-        }
+  const handleRentProvider = async () => {
+    const newRent = {
+      address: address,
+      selectedOption: selectedOption,
     };
-
-    useEffect(() => {
-        if (show) {
-            // Load the Google Maps API script when the modal is shown
-            const script = document.createElement("script");
-            script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&libraries=places`;
-            script.onload = () => setScriptLoaded(true);
-            document.body.appendChild(script);
-        }
-    }, [show]);
-
-    useEffect(() => {
-        if (selectedOption && dataProvider.price) {
-            const cost =
-                parseFloat(selectedOption) * parseFloat(dataProvider.price);
-            setTotalCost(cost);
-        }
-    }, [selectedOption, dataProvider.price]);
-
-    const handleSelect = async (value) => {
-        const results = await geocodeByAddress(value);
-        const latLng = await getLatLng(results[0]);
-        setAddress(value);
-    };
-
-    const handleAddressChange = (event) => {
-        setAddress(event.target.value);
-    };
-
-    return (
-        <>
-            <Modal style={{ marginTop: 100 }} show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Thuê người yêu</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div className="mb-3">
-                        <label className="form-label">
-                            Tên người yêu: {dataProvider.name}
-                        </label>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Chi phí: {dataProvider.price}</label>
-                    </div>
-
-                    <div className="body-add-new">
-                        <label className="form-label">Địa điểm</label>
-                        {scriptLoaded && (
-                            <PlacesAutocomplete
-                                value={address}
-                                onChange={setAddress}
-                                onSelect={handleSelect}
-                            >
-                                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                                    <div>
-                                        <input
-                                            className="form-control"
-                                            {...getInputProps()}
-                                            onChange={handleAddressChange}
-                                        />
-                                        <ul>
-                                            {loading ? <div>Loading...</div> : null}
-                                            {suggestions.map((suggestion) => {
-                                                const style = {
-                                                    backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
-                                                };
-                                                return (
-                                                    <li
-                                                        key={suggestion.placeId}
-                                                        {...getSuggestionItemProps(suggestion, { style })}
-                                                    >
-                                                        {suggestion.description}
-                                                    </li>
-                                                );
-                                            })}
-                                        </ul>
-                                    </div>
-                                )}
-                            </PlacesAutocomplete>
-                        )}
-                    </div>
-
-                    <div className="body-add-new">
-                        <label className="form-label">Thời gian muốn thuê</label>
-                        <select
-                            className="form-select"
-                            value={selectedOption}
-                            onChange={(event) => setSelectedOption(event.target.value)}
-                        >
-                            <option value="">Chọn thời gian</option>
-                            <option value="1">1 giờ</option>
-                            <option value="2">2 giờ</option>
-                            <option value="3">3 giờ</option>
-                            {/* Rest of the options */}
-                        </select>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Tổng số tiền: {totalCost}</label>
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Hủy
-                    </Button>
-                    <Button variant="primary" onClick={handleRentProvider}>
-                        Thuê
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+    setRent(newRent);
+    let data = dispatch(
+      addBooking({ providerId: dataProvider.id, bookingData: newRent })
     );
+
+    if (data) {
+      Swal.fire({
+        icon: "success",
+        title: "Đã thuê! Vui lòng chờ người cho thuê phản hồi.",
+        showConfirmButton: false,
+        timer: 3000,
+        customClass: {
+          popup: "swal2-show",
+        },
+        onOpen: () => {
+          const swalContainer = document.querySelector(".swal2-container");
+          if (swalContainer) {
+            swalContainer.style.zIndex = "100";
+          }
+        },
+      });
+    } else {
+      // Handle failure case
+    }
+  };
+
+  useEffect(() => {
+    if (show) {
+      // Load the Google Maps API script when the modal is shown
+      const script = document.createElement("script");
+      script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&libraries=places`;
+      script.onload = () => setScriptLoaded(true);
+      document.body.appendChild(script);
+    }
+  }, [show]);
+
+  useEffect(() => {
+    if (selectedOption && dataProvider.price) {
+      const cost = parseFloat(selectedOption) * parseFloat(dataProvider.price);
+      setTotalCost(cost);
+    }
+  }, [selectedOption, dataProvider.price]);
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAddress(value);
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
+
+  return (
+    <>
+      <Modal
+        keepMounted
+        open={show}
+        onClose={handleClose}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box sx={style}>
+          <Stack p={2} alignItems={"center"}>
+            <Typography variant="h6" id="modal-title">
+              THUÊ NGƯỜI YÊU
+            </Typography>
+          </Stack>
+          <Divider />
+          <Stack p={2} direction={"column"}>
+            <Grid container rowSpacing={1}>
+              <Grid item xs={8}>
+                <Typography variant="body1" gutterBottom>
+                  Tên:
+                </Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Typography variant="body1" gutterBottom>
+                  {dataProvider.name}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container rowSpacing={1}>
+              <Grid item xs={8}>
+                <Typography variant="body1" gutterBottom>
+                  Thời gian muốn thuê
+                </Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Typography variant="body1" gutterBottom>
+                  <Select
+                    fullWidth
+                    padding={"none"}
+                    value={selectedOption}
+                    onChange={(event) => setSelectedOption(event.target.value)}
+                  >
+                    <MenuItem value="">
+                      <em>Chọn thời gian</em>
+                    </MenuItem>
+                    <MenuItem value=""></MenuItem>
+                    <MenuItem value="1">1 giờ</MenuItem>
+                    <MenuItem value="2">2 giờ</MenuItem>
+                    <MenuItem value="3">3 giờ</MenuItem>
+                    {/* Rest of the options */}
+                  </Select>
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container rowSpacing={1}>
+              <Grid item xs={8}>
+                <Typography variant="body1" gutterBottom>
+                  Chi phí:
+                </Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Typography variant="body1" gutterBottom>
+                  {totalCost}
+                </Typography>
+              </Grid>
+            </Grid>
+            <div className="body-add-new">
+              <label className="form-label">Địa điểm</label>
+              {scriptLoaded && (
+                <PlacesAutocomplete
+                  value={address}
+                  onChange={setAddress}
+                  onSelect={handleSelect}
+                >
+                  {({
+                    getInputProps,
+                    suggestions,
+                    getSuggestionItemProps,
+                    loading,
+                  }) => (
+                    <div>
+                      <input
+                        className="form-control"
+                        {...getInputProps()}
+                        onChange={handleAddressChange}
+                      />
+                      <ul>
+                        {loading ? <div>Loading...</div> : null}
+                        {suggestions.map((suggestion) => {
+                          const style = {
+                            backgroundColor: suggestion.active
+                              ? "#41b6e6"
+                              : "#fff",
+                          };
+                          return (
+                            <li
+                              key={suggestion.placeId}
+                              {...getSuggestionItemProps(suggestion, {
+                                style,
+                              })}
+                            >
+                              {suggestion.description}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  )}
+                </PlacesAutocomplete>
+              )}
+            </div>
+            <textarea name="" id="" cols="30" rows="10" style={{resize: "none", padding:"5px"}} placeholder="Hãy nhắn gì đó ...." ></textarea>
+          </Stack>
+          <Divider />
+          <Box
+            sx={{
+              p: 2,
+              display: "flex",
+              justifyContent: "flex-end",
+              gap: "10px",
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleRentProvider}
+              sx={{
+                bgcolor: "red",
+                color: "white",
+                "&:hover": {
+                  bgcolor: "red",
+                  color: "white",
+                  boxShadow: "none",
+                },
+              }}
+            >
+              Thuê
+            </Button>
+            <Button
+              variant="contained"
+              onClick={handleClose}
+              sx={{
+                bgcolor: "#d4d4d5",
+                "&:hover": {
+                  bgcolor: "#d4d4d5",
+                  boxShadow: "none",
+                },
+              }}
+            >
+              Đóng
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </>
+  );
 };
 
 export default Rent;
