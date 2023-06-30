aefb1b492b66b12e3f0fa784edb508e696ec98c2
quang <ltb01628645546@gmail.com>
2023-06-30 10:35:30 +0700

    fix loi giao dien

--- src/page/user/UserMenu.jsx
+++ src/page/user/UserMenu.jsx
@@ -72,144 +72,90 @@ const UserMenu = ({ anchorUserMenu, setAnchorUserMenu }) => {
   };
 
   return (
-    <>
-      <Menu
-        anchorEl={anchorUserMenu}
-        open={Boolean(anchorUserMenu)}
-        onClose={handleCloseUserMenu}
-        onClick={handleCloseUserMenu}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            bgcolor: "customColorSchemes.backgroundColor",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 50,
-              height: 50,
-              ml: -0.5,
-              mr: 1,
-              mt: 2,
-            },
-            "&:before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 20,
-              width: 10,
-              height: 10,
-              bgcolor: "customColorSchemes.backgroundColor",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-      >
-        <MenuItem>
-          <Box sx={{ display: "flex" }} onClick={handlePage}>
-            <Avatar src={user?.avatar} />
-            <Box>
-              <Typography variant="subtitle1" gutterBottom>
-                {user?.lastname}
-              </Typography>
-              <Typography
-                variant="caption"
-                display="block"
-                gutterBottom
-                color="red"
-              >
-                ID:{user?.email}
-              </Typography>
-              <Typography
-                variant="caption"
-                display="block"
-                gutterBottom
-                color="gray"
-              >
-                Xem trang cá nhân của bạn
-              </Typography>
+      [
+        <Menu
+            key="user-menu"
+            anchorEl={anchorUserMenu}
+            open={Boolean(anchorUserMenu)}
+            onClose={handleCloseUserMenu}
+            onClick={handleCloseUserMenu}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                bgcolor: "customColorSchemes.backgroundColor",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 50,
+                  height: 50,
+                  ml: -0.5,
+                  mr: 1,
+                  mt: 2,
+                },
+                "&:before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 20,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "customColorSchemes.backgroundColor",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem key={1}>
+            <Box sx={{ display: "flex" }} onClick={handlePage}>
+              <Avatar src={user?.avatar} />
+              <Box>
+                <Typography variant="subtitle1" gutterBottom>
+                  {user?.lastname}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {user?.email}
+                </Typography>
+              </Box>
             </Box>
-          </Box>
-        </MenuItem>
-        <Divider />
-        {user?.role?.name === "admin" && (
-          <MenuItem onClick={goAdminPage}>
-            <ListItemIcon>
-              <ManageAccountsIcon />
-            </ListItemIcon>
-            Đến trang quản lý
           </MenuItem>
-        )}
-        {user?.role?.name === "provider" && (
-          <>
-            <MenuItem
-              onClick={() => {
-                navigate("/provider_setting");
-              }}
-            >
-              <ListItemIcon>
-                <ManageAccountsIcon />
-              </ListItemIcon>
-              Đến trang dịch vụ
-            </MenuItem>
-            {profile ? (
-              <MenuItem>
-                <Stack
-                  direction={"row"}
-                  onClick={handleSwitchClick}
-                  alignItems={"center"}
-                  gap={7}
-                >
-                  <Box>Trạng thái</Box>
-
-                  <CustomizedSwitches profile={profile}/>
-                </Stack>
+          <Divider key={2} />
+          <MenuItem key={3} onClick={handleClickOpen}>
+            <ListItemIcon>
+              <HowToRegIcon fontSize="small" />
+            </ListItemIcon>
+            <Typography variant="body1">Register as Service Provider</Typography>
+          </MenuItem>
+          {profile && profile.isProvider && (
+              <MenuItem key={4} onClick={changeProfile}>
+                <ListItemIcon>
+                  <ManageAccountsIcon fontSize="small" />
+                </ListItemIcon>
+                <Typography variant="body1">Change to Customer</Typography>
               </MenuItem>
-            ) : (
-              ""
-            )}
-          </>
-        )}
-        {user?.role?.name === "user" && (
-          <MenuItem onClick={handleClickOpen}>
+          )}
+          {user?.role === "admin" && (
+              <MenuItem key={5} onClick={goAdminPage}>
+                <ListItemIcon>
+                  <ManageAccountsIcon fontSize="small" />
+                </ListItemIcon>
+                <Typography variant="body1">Admin Panel</Typography>
+              </MenuItem>
+          )}
+          <CustomizedSwitches />
+          <Divider key={6} />
+          <MenuItem key={7} onClick={handleLogout}>
             <ListItemIcon>
-              <HowToRegIcon />
+              <Logout fontSize="small" />
             </ListItemIcon>
-            Đăng ký cung cấp dịch vụ
+            <Typography variant="body1">Logout</Typography>
           </MenuItem>
-        )}
-        <Divider />
-
-        <MenuItem onClick={() => navigate("/done")}>
-          <ListItemIcon>
-            <WatchLaterIcon fontSize="small" />
-          </ListItemIcon>
-          Lịch sử giao dịch
-        </MenuItem>
-
-        <MenuItem onClick={changeProfile}>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Cài đặt tài khoản
-        </MenuItem>
-
-        <MenuItem onClick={handleLogout}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Đăng xuất
-        </MenuItem>
-      </Menu>
-      <RegisterService
-        openRegister={openRegister}
-        setOpenRegister={setOpenRegister}
-      />
-    </>
+        </Menu>,
+      ]
   );
 };
 
